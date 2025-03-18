ffe630e8701e86e20e0cb0cc2aa5528dca73fb8e
sheikhahmad <160427855+Ahmadraza08@users.noreply.github.com>
2025-03-18 22:44:06 +0500

    Update Collection.jsx

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -94,7 +94,7 @@ const Collection = () => {
               <input className="w-3" type="radio" value="TV" onChange={toggleCategory} name="category" /> TV
             </label>
             <label className="flex gap-2">
-              <input className="w-3" type="radio" value="FRIDGE" onChange={toggleCategory} name="category" /> FRIDGE
+              <input className="w-3" type="radio" value="FRIDGE" onChange={toggleCategory} name="category" /> FRIDGE & DEEP FREEZER
             </label>
             <label className="flex gap-2">
               <input className="w-3" type="radio" value="WASHING MACHINE" onChange={toggleCategory} name="category" /> WASHING MACHINE
@@ -104,6 +104,9 @@ const Collection = () => {
             </label>
             <label className="flex gap-2">
               <input className="w-3" type="radio" value="AC" onChange={toggleCategory} name="category" /> AC
+            </label>
+            <label className="flex gap-2">
+              <input className="w-3" type="radio" value="WATER DISPENSER" onChange={toggleCategory} name="category" /> WATER DISPENSER
             </label>
             <label className="flex gap-2">
               <input className="w-3" type="radio" value="BIKES" onChange={toggleCategory} name="category" /> BIKES
